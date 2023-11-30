93d5d037007a0f52079bcd1cb65b384a79f2694b
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2023-11-30 12:57:24 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, AgendaService, MonthAgendaService, TimelineViewsService, TimelineMonthService } from '@syncfusion/ej2-angular-schedule';
 
-import { TreeViewModule, AccordionModule, ToolbarModule, ContextMenuModule, BreadcrumbModule, CarouselModule, 
-         TabModule, SidebarModule, MenuModule, AppBarModule } from '@syncfusion/ej2-angular-navigations';
+import { TreeViewModule } from '@syncfusion/ej2-angular-navigations';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,7 @@ import { TreeViewModule, AccordionModule, ToolbarModule, ContextMenuModule, Brea
   ],
   imports: [
     BrowserModule,
-    ScheduleModule, RecurrenceEditorModule, TreeViewModule, AccordionModule, ToolbarModule, ContextMenuModule, BreadcrumbModule, CarouselModule, TabModule, SidebarModule, MenuModule, AppBarModule, 
+    ScheduleModule, RecurrenceEditorModule, TreeViewModule 
   ],
   providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService, MonthAgendaService, TimelineViewsService, TimelineMonthService],
   bootstrap: [AppComponent]
